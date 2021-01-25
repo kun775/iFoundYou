3f99c34b368603b868c4abc8adcc32994e40696c
huangshikun <huangshikun2008@126.com>
2021-01-25 09:01:23 +0800

    添加项目文件。

---
+++ iFoundYou/IFoundYou.Designer.cs
@@ -0,0 +1,168 @@
+﻿
+namespace iFoundYou
+{
+    partial class IFoundYou
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(IFoundYou));
+            this.label_Position = new System.Windows.Forms.Label();
+            this.label_Run = new System.Windows.Forms.Label();
+            this.label_Type = new System.Windows.Forms.Label();
+            this.label_Title = new System.Windows.Forms.Label();
+            this.label_ID = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.Button_Close = new System.Windows.Forms.Button();
+            this.Button_Rename = new System.Windows.Forms.Button();
+            this.Button_Resume = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label_Position
+            // 
+            this.label_Position.AutoSize = true;
+            this.label_Position.Location = new System.Drawing.Point(160, 6);
+            this.label_Position.MinimumSize = new System.Drawing.Size(100, 12);
+            this.label_Position.Name = "label_Position";
+            this.label_Position.Size = new System.Drawing.Size(100, 12);
+            this.label_Position.TabIndex = 15;
+            this.label_Position.Text = "(0, 0)";
+            this.label_Position.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label_Run
+            // 
+            this.label_Run.AutoSize = true;
+            this.label_Run.Location = new System.Drawing.Point(3, 72);
+            this.label_Run.Name = "label_Run";
+            this.label_Run.Size = new System.Drawing.Size(65, 12);
+            this.label_Run.TabIndex = 14;
+            this.label_Run.Text = "运行路径：";
+            this.label_Run.Click += new System.EventHandler(this.Label_Run_Click);
+            // 
+            // label_Type
+            // 
+            this.label_Type.AutoSize = true;
+            this.label_Type.Location = new System.Drawing.Point(3, 50);
+            this.label_Type.Name = "label_Type";
+            this.label_Type.Size = new System.Drawing.Size(65, 12);
+            this.label_Type.TabIndex = 13;
+            this.label_Type.Text = "窗口类型：";
+            // 
+            // label_Title
+            // 
+            this.label_Title.AutoSize = true;
+            this.label_Title.Location = new System.Drawing.Point(3, 28);
+            this.label_Title.Name = "label_Title";
+            this.label_Title.Size = new System.Drawing.Size(65, 12);
+            this.label_Title.TabIndex = 12;
+            this.label_Title.Text = "窗口标题：";
+            // 
+            // label_ID
+            // 
+            this.label_ID.AutoSize = true;
+            this.label_ID.Location = new System.Drawing.Point(3, 6);
+            this.label_ID.Name = "label_ID";
+            this.label_ID.Size = new System.Drawing.Size(65, 12);
+            this.label_ID.TabIndex = 11;
+            this.label_ID.Text = "窗口句柄：";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // Button_Close
+            // 
+            this.Button_Close.Location = new System.Drawing.Point(265, 1);
+            this.Button_Close.Name = "Button_Close";
+            this.Button_Close.Size = new System.Drawing.Size(75, 23);
+            this.Button_Close.TabIndex = 16;
+            this.Button_Close.Text = "关闭进程";
+            this.Button_Close.UseVisualStyleBackColor = true;
+            this.Button_Close.Click += new System.EventHandler(this.Button_Close_Click);
+            // 
+            // Button_Rename
+            // 
+            this.Button_Rename.Location = new System.Drawing.Point(346, 1);
+            this.Button_Rename.Name = "Button_Rename";
+            this.Button_Rename.Size = new System.Drawing.Size(75, 23);
+            this.Button_Rename.TabIndex = 17;
+            this.Button_Rename.Text = "重命名";
+            this.Button_Rename.UseVisualStyleBackColor = true;
+            this.Button_Rename.Click += new System.EventHandler(this.Button_Rename_Click);
+            // 
+            // Button_Resume
+            // 
+            this.Button_Resume.Location = new System.Drawing.Point(427, 1);
+            this.Button_Resume.Name = "Button_Resume";
+            this.Button_Resume.Size = new System.Drawing.Size(75, 23);
+            this.Button_Resume.TabIndex = 18;
+            this.Button_Resume.Text = "恢复";
+            this.Button_Resume.UseVisualStyleBackColor = true;
+            this.Button_Resume.Click += new System.EventHandler(this.Button_Resume_Click);
+            // 
+            // IFoundYou
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(178)))), ((int)(((byte)(203)))), ((int)(((byte)(229)))));
+            this.ClientSize = new System.Drawing.Size(504, 91);
+            this.Controls.Add(this.Button_Resume);
+            this.Controls.Add(this.Button_Rename);
+            this.Controls.Add(this.Button_Close);
+            this.Controls.Add(this.label_Position);
+            this.Controls.Add(this.label_Run);
+            this.Controls.Add(this.label_Type);
+            this.Controls.Add(this.label_Title);
+            this.Controls.Add(this.label_ID);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "IFoundYou";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "我找到你了-获取弹窗信息(按F9暂停，按F10打开运行路径)";
+            this.Load += new System.EventHandler(this.IFoundYou_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label_Position;
+        private System.Windows.Forms.Label label_Run;
+        private System.Windows.Forms.Label label_Type;
+        private System.Windows.Forms.Label label_Title;
+        private System.Windows.Forms.Label label_ID;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button Button_Close;
+        private System.Windows.Forms.Button Button_Rename;
+        private System.Windows.Forms.Button Button_Resume;
+    }
+}
+
---
+++ iFoundYou/IFoundYou.cs
@@ -0,0 +1,267 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+
+namespace iFoundYou
+{
+    enum HotKey
+    {
+        HotKey_ADKillPause = 1001,
+        HotKey_ADKillOpen = 1002
+    };
+    public partial class IFoundYou : Form
+    {
+        [DllImport("user32", SetLastError = true)]
+        public static extern int GetWindowText(IntPtr hWnd, StringBuilder lpString, int nMaxCount);
+        [DllImport("user32.dll")]
+        public static extern int GetClassName(IntPtr hWnd, StringBuilder lpString, int nMaxCount);
+        [DllImport("user32.dll")]
+        internal static extern IntPtr WindowFromPoint(Point Point);
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        public static extern int GetWindowThreadProcessId(IntPtr handle, out uint processId);
+        [DllImport("user32.dll")] //申明API函数
+        public static extern bool RegisterHotKey(IntPtr hWnd, int id, uint fsModifiers, Keys vk);
+        [DllImport("user32.dll")] //申明API函数
+        public static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+        [DllImport("user32.dll")]
+        public static extern bool GetCursorPos(out Point lpPoint);
+        [DllImport("kernel32.dll")]
+        private static extern bool QueryFullProcessImageName(IntPtr hprocess, int dwFlags, StringBuilder lpExeName, out int size);
+        [DllImport("kernel32.dll")]
+        private static extern IntPtr OpenProcess(ProcessAccessFlags dwDesiredAccess, bool bInheritHandle, int dwProcessId);
+        [DllImport("kernel32.dll", SetLastError = true)]
+        private static extern bool CloseHandle(IntPtr hHandle);
+        [Flags]
+        enum ProcessAccessFlags : uint
+        {
+            All = 0x001F0FFF,
+            Terminate = 0x00000001,
+            CreateThread = 0x00000002,
+            VMOperation = 0x00000008,
+            VMRead = 0x00000010,
+            VMWrite = 0x00000020,
+            DupHandle = 0x00000040,
+            SetInformation = 0x00000200,
+            QueryInformation = 0x00000400,
+            QueryLimitedInformation = 0x1000,
+            Synchronize = 0x00100000
+        }
+
+        private string RunPath = "";
+        private bool IsPause = false;
+        private Process ProcessById = null;
+        public IFoundYou()
+        {
+            InitializeComponent();
+            Text = "我找到你了-获取弹窗信息(按F9暂停，按F10打开运行路径)";
+        }
+        private void IFoundYou_Load(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+            try
+            {
+                RegisterHotKey(Handle, (int)HotKey.HotKey_ADKillOpen, 0, Keys.F10);
+                RegisterHotKey(Handle, (int)HotKey.HotKey_ADKillPause, 0, Keys.F9);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("注册快捷键失败:\n" + ex.Message);
+            }
+        }
+        // 打开路径
+        private void OpenPath()
+        {
+            if (string.IsNullOrEmpty(RunPath))
+            {
+                return;
+            }
+
+            string FilePath = Path.GetDirectoryName(RunPath);
+            string FileName = Path.GetFileName(RunPath);
+            Process.Start("Explorer", "/select," + FilePath + "\\" + FileName);
+        }
+        private void Pause()
+        {
+            IsPause = !IsPause;
+            if (IsPause)
+            {
+                Text = "我找到你了-获取弹窗信息(按F9继续，F10打开运行路径)";
+            }
+            else
+            {
+                Text = "我找到你了-获取弹窗信息(按F9暂停，按F10打开运行路径)";
+            }
+        }
+        private void ProcessHotkey(Message m) //按下设定的键时调用该函数
+        {
+            int id = m.WParam.ToInt32();
+            switch (id)
+            {
+                case (int)HotKey.HotKey_ADKillOpen: OpenPath(); break;
+                case (int)HotKey.HotKey_ADKillPause: Pause();   break;
+            }
+        }
+        protected override void WndProc(ref Message m)//监视Windows消息
+        {
+            const int WM_HOTKEY = 0x0312;//如果m.Msg的值为0x0312那么表示用户按下了热键
+            switch (m.Msg)
+            {
+                //按下热键时调用ProcessHotkey()函数
+                case WM_HOTKEY: ProcessHotkey(m); break;
+            }
+            base.WndProc(ref m); //将系统消息传递自父类的WndProc
+        }
+
+        private void ADKillForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            UnregisterHotKey(Handle, (int)HotKey.HotKey_ADKillPause);
+            UnregisterHotKey(Handle, (int)HotKey.HotKey_ADKillOpen);
+        }
+        private static string GetExecutablePath(Process Process)
+        {
+            //If running on Vista or later use the new function
+            if (Environment.OSVersion.Version.Major >= 6)
+            {
+                return GetExecutablePathAboveVista(Process.Id);
+            }
+
+            return Process.MainModule.FileName;
+        }
+        private static string GetExecutablePathAboveVista(int ProcessId)
+        {
+            StringBuilder buffer = new StringBuilder(256);
+            IntPtr hprocess = OpenProcess(ProcessAccessFlags.QueryLimitedInformation, false, ProcessId);
+            if (hprocess != IntPtr.Zero)
+            {
+                try
+                {
+                    int size = buffer.Capacity;
+                    if (QueryFullProcessImageName(hprocess, 0, buffer, out size))
+                    {
+                        return buffer.ToString();
+                    }
+                }
+                finally
+                {
+                    CloseHandle(hprocess);
+                }
+            }
+            return Marshal.GetLastWin32Error().ToString();
+        }
+        private void MouseMoveEvent()
+        {
+            GetCursorPos(out Point point);
+            int x = point.X;
+            int y = point.Y;
+            label_Position.Text = $"({x}, {y})";
+            Point p = new Point(x, y);
+            IntPtr intPtr = WindowFromPoint(p);//得到窗口句柄
+            StringBuilder title = new StringBuilder(256);
+            GetWindowText(intPtr, title, title.Capacity);//得到窗口的标题
+            StringBuilder className = new StringBuilder(256);
+            GetClassName(intPtr, className, className.Capacity);//得到窗口的句柄
+
+            label_ID.Text = $"窗口句柄：{intPtr}";
+            label_Title.Text = $"窗口标题：{title}";
+            label_Type.Text = $"窗口类型：{className}";
+            GetWindowThreadProcessId(intPtr, out uint processId);
+            ProcessById = Process.GetProcessById((int)processId);
+            try
+            {
+                RunPath = GetExecutablePath(ProcessById);
+                label_Run.Text = $"运行路径：{RunPath}";
+            }
+            catch (Exception ex)
+            {
+                RunPath = "";
+                label_Run.Text = ex.Message;
+            }
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            if (IsPause)
+                return;
+            MouseMoveEvent();
+        }
+        // 关闭进程
+        private void Button_Close_Click(object sender, EventArgs e)
+        {
+            if (ProcessById != null)
+            {
+                ProcessById.Kill();
+                ProcessById = null;
+
+                MessageBox.Show("关闭成功", "提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+        // 重命名
+        private void Button_Rename_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(RunPath))
+                return;
+
+            DialogResult dr = MessageBox.Show("" +
+                "重命名会备份弹窗文件，为了防止下载新的弹窗文件，会在原地创建同名文件\n" +
+                "此功能一般用于单独弹窗进程，如果弹窗功能集成在软件里，重命名会导致软件无法使用。\n" +
+                "重命名前请先关闭弹窗进程。重命名后，可用恢复功能恢复原先文件。\n" +
+                "是否确认重命名？", 
+                "！！注意 ！！", 
+                MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            if (dr == DialogResult.No)
+                return;
+
+            try
+            {
+                File.Move(RunPath, RunPath + "_bak");
+                MessageBox.Show($"重命名成功！\n{RunPath}_bak", "提示",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+                using (FileStream fs = File.Open(RunPath, FileMode.Create))
+                {
+                    fs.WriteByte(0);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"重命名出错！\n{ex.Message}", "提示", 
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+        // 恢复
+        private void Button_Resume_Click(object sender, EventArgs e)
+        {
+            if (!File.Exists(RunPath + "_bak"))
+            {
+                MessageBox.Show($"备份路径不存在，无法恢复！\n{RunPath}_bak", "错误", 
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            try
+            {
+                if (File.Exists(RunPath))
+                    File.Delete(RunPath);
+
+                File.Move(RunPath + "_bak", RunPath);
+                MessageBox.Show($"恢复成功！\n{RunPath}", "提示", 
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"恢复错误！\n{ex.Message}", "提示", 
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void Label_Run_Click(object sender, EventArgs e)
+        {
+            OpenPath();
+        }
+    }
+}
